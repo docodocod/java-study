0cf25eeedf0bd8267ad2f8df0928b4f8e486bc4d
donga <dongan0618@naver.com>
2025-04-02 11:34:50 +0900

    ch2 self check

---
+++ src/ch02/IO.java
@@ -0,0 +1,12 @@
+package ch02;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class IO {
+    public static void main(String[] args) throws IOException {
+        Scanner scan=new Scanner(System.in);
+        System.out.println(scan.nextLine());
+        System.out.println(scan.nextLine());
+        System.out.println(scan.nextLine());
+    }
+}
